a26a0489f4d75dca5401b0b0c31b5d65a3f5657c
Hamifthi <hamedonil@yahoo.com>
2018-03-25 15:29:40 +0430

    evaluating kfolds on ANN to recieve different accuracy

---
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/buildFunction.py
@@ -0,0 +1,14 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+def buildClassifier ():
+    classifier = Sequential()
+    classifier.add(Dense(activation = "relu", input_dim = 11, units = 6, kernel_initializer = 'uniform'))
+    classifier.add(Dense(activation = "relu", units = 6, kernel_initializer = 'uniform'))
+    classifier.add(Dense(activation = "sigmoid", units = 1, kernel_initializer = 'uniform'))
+    classifier.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+    return classifier
+
+if __name__ == '__main__':
+    print('Working fine')
