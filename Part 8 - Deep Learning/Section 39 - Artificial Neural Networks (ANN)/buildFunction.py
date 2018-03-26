a7ed346d4245d6bae2092e5b40f111e30e6c75f3
Hamifthi <hamedonil@yahoo.com>
2018-03-26 16:53:08 +0430

    also change the build in classifier

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/buildFunction.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/buildFunction.py
@@ -2,12 +2,12 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-def buildClassifier ():
+def buildClassifier (optimizer):
     classifier = Sequential()
     classifier.add(Dense(activation = "relu", input_dim = 11, units = 6, kernel_initializer = 'uniform'))
     classifier.add(Dense(activation = "relu", units = 6, kernel_initializer = 'uniform'))
     classifier.add(Dense(activation = "sigmoid", units = 1, kernel_initializer = 'uniform'))
-    classifier.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+    classifier.compile(loss = 'binary_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
     return classifier
 
 if __name__ == '__main__':
